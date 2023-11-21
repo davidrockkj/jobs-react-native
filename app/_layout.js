cf525445b073386918dc69a29e2e5c9b5bcc0fb3
davidrockkj <davidrockkj@gmail.com>
2023-11-21 15:23:24 -0300

    Inicializando projeto já estruturado

---
+++ app/_layout.js
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router'
+// Componente pré-construído para renderizar um navegador de pilha nativo
+
+const Layout = () => {
+  return <Stack />;
+}
+
+export default Layout;
